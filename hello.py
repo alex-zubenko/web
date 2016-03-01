b857558062a7d500684479ec3d26e0d4f300bb4d
Aleksey Zubenko <alex-zubenko@yandex.ru>
2016-03-01 11:39:11 +0300

    changed hginx conf

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
 def app(environ, start_response):
     path = environ.get('PATH_INFO')
     query = environ.get('QUERY_STRING')
-    print(query)
-    response_body = "Index"
+    query = query.replace('&',"\r\n")
+    response_body = query
     status = "200 OK"
     response_headers = [("Content-Type", "text/plain"),("Content-Length", str(len(response_body)))]
     start_response(status, response_headers)

e4ac1e98a7263b2eca8514430e8a89bedb55e5b0
Aleksey Zubenko <alex-zubenko@yandex.ru>
2016-03-04 07:05:42 +0300

    .

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
-#cd /home/box/web/ask
-#sudo gunicorn -b 0.0.0.0:8000 ask.wsgi:application
+#sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+#sudo /etc/init.d/gunicorn restart
+cd ~/Рабочий\ стол/web/ask
+sudo gunicorn -w 1 -b 0.0.0.0:8000 ask.wsgi:app

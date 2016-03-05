13083436d20700d3a7b5d9a07ccb0d8706b09406
Aleksey Zubenko <alex-zubenko@yandex.ru>
2016-03-05 08:52:54 +0300

    models added (2.3-10 lesson)

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Question(models.Model):
+	title = CharField(max_length=255)
+	text = TextField()
+	added_at = DateField()
+	rating = IntegerField()
+	author = models.ForeignKey(User, null=False, on_delete=models.NO_ACTION)
+	likes = models.OneToManyField(User)
+	def __unicode__:
+		return self.title
+
+class Answer(models.Model):
+	text = TextField()
+	added_at = DateField()
+	question = TextField()
+	author = models.ForeignKey(User, null=False, on_delete=models.NO_ACTION)
--- init.sh
+++ init.sh
@@ -4,4 +4,6 @@ sudo /etc/init.d/nginx restart
 #sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
 #cd /home/box/web/
-sudo gunicorn -w 1 -b 0.0.0.0:8000 ask.wsgi --pythonpath '/home/box/web/ask' --daemon &
+#sudo gunicorn -w 1 -b 0.0.0.0:8000 ask.wsgi --pythonpath '/home/box/web/ask' --daemon &
+sudo gunicorn -w 1 -b 0.0.0.0:8000 ask.wsgi --pythonpath '/home/alezu/Рабочий\ стол/web/ask' --daemon &
+

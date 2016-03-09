55f05aafc9959cac816ea7cc1dd08f0d1a1e3a94
Aleksey Zubenko <alex-zubenko@yandex.ru>
2016-03-09 11:57:06 +0300

    ...

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -15,5 +15,5 @@ class Answer(models.Model):
 	text = models.TextField()
 	added_at = models.DateField(null=True)
 	text = models.TextField()
-	question = ForeignKey(Question)
+	question = models.ForeignKey(Question)
 	author = models.ForeignKey(User, null=False, on_delete=models.DO_NOTHING)

01590a89b64033b1b8ea38934adaeddf21dbb99d
Aleksey Zubenko <alex-zubenko@yandex.ru>
2016-03-09 20:34:55 +0300

    ...

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -35,7 +35,7 @@ def popular(request, *args, **kwargs):
 @require_GET
 def question(request, id):
 	post = get_object_or_404(Question, id=id)
-	answers = answer.question_set.all()
+	answers = Answer.question_set.all()
 	return render(request, 'question.html', {
 		'post':	post,
 		'answers': answers,

c466858d683a460b71332962364d8c6ec77e83db
Aleksey Zubenko <alex-zubenko@yandex.ru>
2016-03-09 11:40:26 +0300

    ...

--- init.sh
+++ init.sh
@@ -6,6 +6,8 @@ sudo /etc/init.d/nginx restart
 #cd /home/box/web/
 sudo /etc/init.d/mysql start
 mysql -uroot -e "create database web"
+mysql -uroot -e "CREATE USER box@localhost IDENTIFIED BY user"
+mysql -uroot -e "GRANT ALL ON web.* TO box@localhost"
 sudo gunicorn -w 1 -b 0.0.0.0:8000 ask.wsgi --pythonpath '/home/box/web/ask' --daemon &
 #sudo gunicorn -w 1 -b 0.0.0.0:8000 ask.wsgi --pythonpath '/home/alezu/Рабочий\ стол/web/ask' --daemon &
 

9a65afaeaf75931679605760bd4230c890a2e4b8
Aleksey Zubenko <alex-zubenko@yandex.ru>
2016-03-09 16:04:43 +0300

    ...

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -26,26 +26,9 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
-        ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                "allauth.account.context_processors.account",
-                "allauth.socialaccount.context_processors.socialaccount",
-            ],
-        },
-    },
-]
-
+TEMPLATE_DIRS = (
+    os.path.join(os.path.dirname(__file__), 'templates').replace('\\','/'),
+)
 
 # Application definition
 

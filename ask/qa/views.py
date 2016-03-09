8aa6b04cee9f15b041eed9b758db9ecd426eeac3
Aleksey Zubenko <alex-zubenko@yandex.ru>
2016-03-09 18:07:00 +0300

    ...

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -7,10 +7,10 @@ def test(request, *args, **kwargs):
 	return HttpResponse('OK')
 
 def home(request):
-	posts = Question.objects.order_by("-id")
+	questions = Question.objects.order_by("-id")
 	limit = request.GET.get('limit', 10)
 	page = request.GET.get('page', 1)
-	paginator = Paginator(posts, limit)
+	paginator = Paginator(questions, limit)
 	paginator.baseUrl = '/?page='
 	page = paginator.page(page)
 	return render(request, 'home.html', {

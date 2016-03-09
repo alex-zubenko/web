f37edfff4fe94c79aa1c64d92bd6cabceabffd2c
Aleksey Zubenko <alex-zubenko@yandex.ru>
2016-03-09 11:34:42 +0300

    ...

--- init.sh
+++ init.sh
@@ -4,6 +4,8 @@ sudo /etc/init.d/nginx restart
 #sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
 #cd /home/box/web/
+sudo /etc/init.d/mysql start
+mysql -uroot -e "create database web"
 sudo gunicorn -w 1 -b 0.0.0.0:8000 ask.wsgi --pythonpath '/home/box/web/ask' --daemon &
 #sudo gunicorn -w 1 -b 0.0.0.0:8000 ask.wsgi --pythonpath '/home/alezu/Рабочий\ стол/web/ask' --daemon &
 

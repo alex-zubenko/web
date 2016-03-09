8c81150dd2525612f55c70056c678c944011adda
Aleksey Zubenko <alex-zubenko@yandex.ru>
2016-03-09 13:17:02 +0300

    ...

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -12,7 +12,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'ask.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-#    url(r'^admin/', include(admin.site.urls),name='home'),
+    url(r'^admin/', include(admin.site.urls),name='home'),
     url(r'^(\?page=\d\d?)?$', home),
     url(r'^signup/?$', test),
     url(r'^question/(\d+)',popular),

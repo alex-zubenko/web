82816df1959e033b46df9c545e0bd43e2fe984c1
Aleksey Zubenko <alex-zubenko@yandex.ru>
2016-03-09 22:58:29 +0300

    ...

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -51,7 +51,7 @@ def add_question(request):
             post = form.save(commit=False)
             post.added_at = timezone.now()
             post.save()
-            return HttpResponseRedirect('/question/' + request.post.get('id'))
+            return HttpResponseRedirect('/question/' + request.POST.get('id'))
     else:
         form = AskForm()
     return render(request, 'add_question.html', {'form': form})

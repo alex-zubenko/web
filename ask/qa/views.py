9d5e896ebac035bc79edd8ab23226ffc4d9d0425
Aleksey Zubenko <alex-zubenko@yandex.ru>
2016-03-09 14:13:20 +0300

    ...

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Question
+from qa.models import Question, Answer
 from django.http import HttpResponse
 from django.core.paginator import Paginator
 

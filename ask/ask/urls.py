37808d89b71bc51d35c91b804901c8bf8241591c
Aleksey Zubenko <alex-zubenko@yandex.ru>
2016-03-11 10:35:58 +0300

    Auth second try...

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from qa.views import home, question, add_question, add_answer, login, popular, register
+from qa.views import home, question, add_question, add_answer, do_login, popular, register
 
 admin.autodiscover()
 
@@ -13,7 +13,7 @@ urlpatterns = patterns('',
     url(r'^$', home,name='home'),
     url(r'^signup/?$', register),
     url(r'^register/?$', register),
-    url(r'^do_login/?$', login),
+    url(r'^login/?$', do_login),
     url(r'^question/(\d+)/',question, name="question"),
     url(r'^ask/$',add_question),
     url(r'^answer/$',add_answer),
